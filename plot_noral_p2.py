c55a307efb5734d18ece1fbc6c1bae978435a464
wym <dxdbwym@163.com>
2021-12-12 16:09:39 +0800

    modify figure

--- plot_noral_p2.py
+++ plot_noral_p2.py
@@ -13,9 +13,15 @@ for col in k2.columns:
 
 
 
-fig, ax = plt.subplots(2, 1, figsize=(10, 15), sharex='all')
+fig, ax = plt.subplots(1, 2, figsize=(14, 6), sharex='all')
 t = [i for i in range(1, k2.shape[0]+1)]
-bs = list(record_k2.keys())
+b_s = list(record_k2.keys())
+bs = []
+for i in range(len(b_s)):
+
+
+
+
 for b in bs:
     ax[0].plot(t, record_k2[b], 'o-', markersize=0.5, lw=1, label='b is '+str(b))
     ax[1].plot(t, record_k3[b], 'o-', markersize=0.5, lw=1, label='b is '+str(b))
@@ -26,8 +32,12 @@ ax[1].set_yscale('log')
 # ax[1].legend(loc=(.05, -.4), ncol=3)
 ax[0].set_ylabel('b')
 ax[1].set_ylabel('b')
+
+ax[0].set_title('the time evolution of the fraction of cooperators c(t) when k = 2')
+ax[1].set_title('the time evolution of the fraction of cooperators c(t) when k = 3')
+
 plt.xlim(1)
 plt.xscale('log')
 plt.xlabel('t')
-plt.savefig('normal_pic_part2.png')
+# plt.savefig('normal_pic_part2_horizontal.png')
 plt.show()
