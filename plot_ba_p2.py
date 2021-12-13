44c846749d27743099de323c6fe05c23beb96c93
KyrieW <dxdbwym@163.com>
2021-12-13 17:38:41 +0800

    重新画图，添加分布图 整理repo

--- plot_ba_p2.py
+++ plot_ba_p2.py
@@ -13,10 +13,10 @@ for col in k2.columns:
 
 
 # horizontal
-fig, ax = plt.subplots(1, 2, figsize=(14, 6), sharey='all')
+# fig, ax = plt.subplots(1, 2, figsize=(14, 6), sharey='all')
 
 # vertical
-# fig, ax = plt.subplots(2, 1, figsize=(8, 12), sharex='all')
+fig, ax = plt.subplots(2, 1, figsize=(8, 12), sharex='all')
 
 
 t = [i for i in range(1, k2.shape[0]+1)]
@@ -27,55 +27,61 @@ for b_ in b_s:
 
 
 for i in range(len(bs)):
-    ax[0].plot(t, record_k2[b_s[i]], 'o-', markersize=0.5, lw=1, label='b is '+bs[i])
-    ax[1].plot(t, record_k3[b_s[i]], 'o-', markersize=0.5, lw=1, label='b is '+bs[i])
+    ax[0].plot(t, record_k2[b_s[i]], 'o-', markersize=0.5, lw=1, label='b is '+bs[i], clip_on=False)
+    ax[1].plot(t, record_k3[b_s[i]], 'o-', markersize=0.5, lw=1, label='b is '+bs[i], clip_on=False)
 
 
 # horizontal
+# ax[0].set_yscale('log')
+# ax[1].set_yscale('log')
+
+# # ax[1].legend(loc=(.05, -.4), ncol=3)
+# ax[0].set_ylabel('<c>')
+# ax[1].set_ylabel('<c>')
+
+# ax[0].set_title('The time evolution of the fraction of cooperators c(t)\n in BA when k = 2')
+# ax[1].set_title('The time evolution of the fraction of cooperators c(t)\n in BA when k = 3')
+
+# ax[0].set_xscale('log')
+# ax[1].set_xscale('log')
+
+# ax[0].set_xlim(1, 10000)
+# ax[1].set_xlim(1, 10000)
+
+# ax[0].set_ylim(0, 1)
+# ax[1].set_ylim(0, 1)
+
+# ax[0].set_xlabel('t')
+# ax[1].set_xlabel('t')
+
+# ax[1].legend(loc=2, bbox_to_anchor=(1,1), borderaxespad=0.)
+# plt.savefig('ba_pic_part2_horizontal.png')
+
+# vertical
 ax[0].set_yscale('log')
 ax[1].set_yscale('log')
 
-# ax[1].legend(loc=(.05, -.4), ncol=3)
+
 ax[0].set_ylabel('<c>')
 ax[1].set_ylabel('<c>')
 
-ax[0].set_title('the time evolution of the fraction of cooperators c(t) in BA when k = 2')
-ax[1].set_title('the time evolution of the fraction of cooperators c(t) in BA when k = 3')
+ax[0].set_title('The time evolution of the fraction of cooperators c(t)\n in BA when k = 2')
+ax[1].set_title('The time evolution of the fraction of cooperators c(t)\n in BA when k = 3')
 
 ax[0].set_xscale('log')
 ax[1].set_xscale('log')
 
-ax[0].set_xlim(1)
-ax[1].set_xlim(1)
+ax[0].set_xlim(1, 10000)
+ax[1].set_xlim(1, 10000)
+
+ax[0].set_ylim(0, 1)
+ax[1].set_ylim(0, 1)
 
 ax[0].set_xlabel('t')
 ax[1].set_xlabel('t')
 
-ax[1].legend(loc=2, bbox_to_anchor=(1,1), borderaxespad=0.)
-plt.savefig('normal_pic_part2_horizontal.png')
-
-# vertical
-# ax[0].set_yscale('log')
-# ax[1].set_yscale('log')
-
-
-# ax[0].set_ylabel('<c>')
-# ax[1].set_ylabel('<c>')
-#
-# ax[0].set_title('The time evolution of the fraction of cooperators c(t) in BA when k = 2')
-# ax[1].set_title('The time evolution of the fraction of cooperators c(t) in BA when k = 3')
-#
-# ax[0].set_xscale('log')
-# ax[1].set_xscale('log')
-#
-# ax[0].set_xlim(1)
-# ax[1].set_xlim(1)
-#
-# ax[0].set_xlabel('t')
-# ax[1].set_xlabel('t')
-#
-# ax[1].legend(bbox_to_anchor=(.9, -.1), borderaxespad=0., ncol=5)
-plt.savefig('ba_pic_part2_horizontal.png')
+ax[1].legend(bbox_to_anchor=(.9, -.1), borderaxespad=0., ncol=5)
+plt.savefig('ba_pic_part2_vertical.png')
 
 
 plt.show()
